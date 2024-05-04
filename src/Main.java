00189536f515d1bd293ed6f352e5bdf063c850fa
dc0d3m3 <162016794+dc0d3m3@users.noreply.github.com>
2024-05-04 12:36:50 +0200

    4

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,29 @@ import company.beans.Person;
 import company.exceptions.InvalidAgeException;
 import company.interfaces.Messenger;
 import company.implementations.EmailMessenger;
+import company.utils.MathUtils;
 
 public class Main {
     public static void main(String[] args) {
+        // Tworzenie tablicy osób
+        Person[] people = new Person[5];
+        int b = 10;  // Stała wartość do obliczeń
+
         try {
-            Person person = new Person("Wiesław Paleta", 58);
-            System.out.println("Person: " + person.getName() + ", Age: " + person.getAge());
+            // Tworzenie instancji Person i przypisywanie do tablicy
+            people[0] = new Person("Wiesław Paleta", 58);
+            people[1] = new Person("John Rambo", 75);
+            people[2] = new Person("Jim Beam", 18);
+            people[3] = new Person("Forrest Gump", 22);
+            people[4] = new Person("Jack Sparrow", 35);
 
             Messenger emailMessenger = new EmailMessenger();
-            emailMessenger.sendMessage("Welcome to the system, " + person.getName());
+
+            // Przetwarzanie i wyświetlanie wiadomości dla każdej osoby
+            for (Person person : people) {
+                int ageSum = MathUtils.add(person.getAge(), b);
+                emailMessenger.sendMessage("Hello " + person.getName() + "! Your age plus " + b + " is " + ageSum + ".");
+            }
         } catch (InvalidAgeException e) {
             System.out.println("Error: " + e.getMessage());
         }
