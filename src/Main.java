73a96a90649021c2cbb3a5f540956499b8ceb0dd
dc0d3m3 <162016794+dc0d3m3@users.noreply.github.com>
2024-05-04 13:01:17 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import company.beans.Person;
 import company.exceptions.InvalidAgeException;
 import company.interfaces.Messenger;
 import company.implementations.EmailMessenger;
+import company.utils.MathUtils;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,8 +10,12 @@ public class Main {
             Person person = new Person("Wiesław Paleta", 58);
             System.out.println("Person: " + person.getName() + ", Age: " + person.getAge());
 
+            // Dodawanie dwóch liczb i wypisywanie wyniku
+            int sum = MathUtils.add(5, 3);
+            System.out.println("Sum of 5 and 3 is: " + sum);
+
             Messenger emailMessenger = new EmailMessenger();
-            emailMessenger.sendMessage("Welcome to the system, " + person.getName());
+            emailMessenger.sendMessage("Welcome to the system, " + person.getName() + ". The sum is: " + sum);
         } catch (InvalidAgeException e) {
             System.out.println("Error: " + e.getMessage());
         }
